aa1beabe46a1ece09a9d74bb1f04b0e7f7f6bb8f
FTAigris <67989874+FTAigris@users.noreply.github.com>
2026-08-28 01:39:51 -0400

    Construir consola con Vista previa y Programa

--- StudioPad/Views/CameraPreview.swift
+++ StudioPad/Views/CameraPreview.swift
@@ -5,6 +5,18 @@ import SwiftUI
 struct CameraPreview: UIViewRepresentable {
     let model: CameraStudioModel
 
+    final class Coordinator {
+        let model: CameraStudioModel
+
+        init(model: CameraStudioModel) {
+            self.model = model
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(model: model)
+    }
+
     func makeUIView(context: Context) -> MTHKView {
         let view = MTHKView(frame: .zero)
         view.videoGravity = AVLayerVideoGravity.resizeAspectFill
@@ -13,5 +25,8 @@ struct CameraPreview: UIViewRepresentable {
     }
 
     func updateUIView(_ uiView: MTHKView, context: Context) {}
-}
 
+    static func dismantleUIView(_ uiView: MTHKView, coordinator: Coordinator) {
+        coordinator.model.detachPreview(uiView)
+    }
+}
